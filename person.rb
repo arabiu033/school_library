5996bcf96f7755c906a859084bf35c8fa975e658
Aminu Rabiu <arabiu033@gmail.com>
2022-12-20 16:54:41 +0100

    Modified the Person class to inherit from Nameable class

--- person.rb
+++ person.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
+require_relative('./nameable')
 
-class Person
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..10_000)
     @name = name
     @age = age
@@ -14,11 +16,14 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
-  
+
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
-  
 end
