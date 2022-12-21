5d03071e32e07d08a59d9077583072ce5f6643dc
Aminu Rabiu <arabiu033@gmail.com>
2022-12-21 14:14:25 +0100

    Modified Student class to associate it with classroom

--- student.rb
+++ student.rb
@@ -2,9 +2,16 @@
 require_relative('./person')
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission)
+    self.classroom = classroom
+  end
+
+  def classroom=(classroom)
     @classroom = classroom
+    classroom.students.push(self) unless students.students.include?(self)
   end
 
   def play_hooky
