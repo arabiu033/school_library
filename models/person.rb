b900c47be03cd1ff46520afa58df134d7e872e13
Aminu Rabiu <arabiu033@gmail.com>
2022-12-22 14:54:19 +0100

    Re-arrange the files to another directory

---
+++ models/person.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+require_relative('./nameable')
+
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
+
+  def initialize(age, name = 'unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..10_000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.person = self
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
---
+++ models/student.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require_relative('./person')
+require_relative('./classroom')
+
+class Student < Person
+  attr_reader :classroom
+
+  def initialize(age, name, parent_permission, classroom: nil)
+    super(age, name, parent_permission: parent_permission)
+    self.classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless !classroom || classroom.students.include?(self)
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+end
