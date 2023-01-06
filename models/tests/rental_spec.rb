ff7ae75f2cfb28749212e6e1491273b037d077bc
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-06 10:59:46 +0100

    Add test for rentals

---
+++ models/tests/rental_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../student'
+require_relative '../book'
+require_relative '../rentals'
+​
+describe Rentals do
+​
+  it 'should accept and return attributes passed' do
+    student = Student.new(21, 'Korra', true)
+    book = Book.new('Dune', 'Korra')
+    rental = Rentals.new(student, book, '2020')
+    expect(rental.person).to be student
+  end
+​
+  it 'should retuen the object state in hash' do
+    student = Student.new(21, 'Korra', true)
+    book = Book.new('Dune', 'Korra')
+    rental = Rentals.new(student, book, '2020').to_hash
+    expect(rental[:p]).to eql student.to_hash
+  end
+end
